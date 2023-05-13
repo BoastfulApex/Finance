85d22d12a2019dcecb6e5634b31b717ac4f7ce0d
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-05-13 11:47:16 +0500

    fixed

--- statistika/serializers.py
+++ statistika/serializers.py
@@ -16,8 +16,8 @@ class TypeSerializer(serializers.ModelSerializer):
 
 
 class CompanySerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-    type = TypeSerializer()
+    # category = CategorySerializer()
+    # type = TypeSerializer()
 
     class Meta:
         model = Company
